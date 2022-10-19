964494ed99f2d15a8daea07f8400df70454b20af
Gustavo Franco <rfrancogustavo@gmail.com>
2022-10-19 19:33:21 -0300

    Implementação até possibilidade de colocar peças no tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import chess.pieces.Rook;
+import chess.pieces.King;
 
 public class ChessMatch {
 
@@ -8,6 +10,7 @@ public class ChessMatch {
 
     public ChessMatch() {
         this.board = new Board(8,8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces(){
@@ -20,5 +23,15 @@ public class ChessMatch {
         }
 
         return mat;
+    }
+
+    private void placeNewPiece(char column, int row, ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
+    private void initialSetup(){
+        placeNewPiece('b', 6 ,new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
     }
 }
